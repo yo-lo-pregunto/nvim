0e0cae1c44e5d643cf9a12d27018913ca6e2ca78
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2025-07-11 14:27:55 -0600

    [core] rename lsp -> config
    Signed-off-by: yo-lo-pregunto <junjo.mendoza11@gmail.com>

--- lua/junjo/core/init.lua
+++ lua/junjo/core/init.lua
@@ -1,5 +1,5 @@
 require 'junjo.core.opts'
+require 'junjo.core.config'
+require 'junjo.core.lazy'
 require 'junjo.core.keys'
 require 'junjo.core.autocmds'
-require 'junjo.core.lsp'
-require 'junjo.core.lazy'
