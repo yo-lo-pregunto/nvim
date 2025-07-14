9fc79beb09d696a21a3bf0855070272649ed7db7
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2025-07-13 22:30:40 -0600

    [lsp] fix bug where lsp where not being activated
    Signed-off-by: yo-lo-pregunto <junjo.mendoza11@gmail.com>

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,34 @@
+vim.lsp.config('*', require('blink.cmp').get_lsp_capabilities())
+
+--- Walk over all files in a directory and return file names without extension
+--- @param dir string: Absolute path to the folder (e.g., "/home/user/.config/nvim/lua/lsp")
+--- @return table: List of file names (no extension)
+local function get_lsp_file_names(dir)
+  local stat = vim.uv or vim.loop
+  local result = {}
+
+  local function scan(path)
+    local fd = stat.fs_scandir(path)
+    if not fd then return end
+
+    while true do
+      local name, t = stat.fs_scandir_next(fd)
+      if not name then break end
+
+      if t == "file" then
+        local file = name:match("^(.*)%.%w+$") or name -- strip extension
+        table.insert(result, file)
+      end
+    end
+  end
+
+  scan(dir)
+  return result
+end
+
+local servers = get_lsp_file_names(vim.fn.stdpath('config') .. '/lsp/')
+
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, {})
+  vim.lsp.enable(server, true)
+end
