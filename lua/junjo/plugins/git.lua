a36f48689f7d17c8ae6ba030339e2c9aadaaf98a
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2025-07-16 10:29:13 -0600

    [plugins] Gitsigns config updated
    Signed-off-by: yo-lo-pregunto <junjo.mendoza11@gmail.com>

--- lua/junjo/plugins/git.lua
+++ lua/junjo/plugins/git.lua
@@ -58,6 +58,7 @@ return {
       preview_config = {
         -- Options passed to nvim_open_win
         style = 'minimal',
+        border = 'rounded',
         relative = 'cursor',
         row = 0,
         col = 1,
